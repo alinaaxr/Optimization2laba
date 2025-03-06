a0389eae2e014f936bf9fc1f0d449ada39c9538f
Алина <alinakhamatvalieva@gmail.com>
2025-03-06 12:08:07 +0500

    1

--- Optimization2laba.cpp
+++ Optimization2laba.cpp
@@ -3,12 +3,12 @@
 #include <cmath>
 using namespace std;
 
-double epsilon1 = 0.01;
+double epsilon1 = 0.01;  //точности решений
 double epsilon2 = 0.01;
 double fi = 10;  //значение штрафной функции в текущей точке
 double r = 10;  //штрафной коэффициент
 double C = 10;  //число для увеличения параметра штрафа
-int k = 0;
+int k = 0;  //номер итерации
 
 struct Point
 {
@@ -16,98 +16,96 @@ struct Point
     double x2;
 };
 
-double function(Point X) {
-    return 2 * pow(X.x1 - 1, 2) + 3 * pow(X.x2 - 3, 2);
+double function(Point x) {  //исходная функция
+    return 2 * pow(x.x1 - 1, 2) + 3 * pow(x.x2 - 3, 2);
 }
 
-double H(Point X) {  //равенство
-    return X.x1 + X.x2 - 6;
+double H(Point x) {  //равенство
+    return x.x1 + x.x2 - 6;
 }
 
-double G(Point X) { //неравенство
-    return X.x1 * X.x1 + X.x2 * X.x2 - 10;
+double G(Point x) { //неравенство
+    return x.x1 * x.x1 + x.x2 * x.x2 - 10;
 }
 
-double F(Point X, double r) {  //штрафная функция
-    return (H(X) * H(X) / (2 * r) + r / G(X)); // Исправлено знак в штрафной функции
+double F(Point x, double r) {  //штрафная функция
+    return (H(x) * H(x) / (2 * r) + r / G(x));
 }
 
-double P(Point X, double r) {  //вспомогательная функция
-    return function(X) + F(X, r);
+double P(Point x, double r) {  //вспомогательная функция
+    return function(x) + F(x, r);
 }
 
-Point gradientF(Point X) {
-    return{ 4 * (X.x1 - 1), 6 * (X.x2 - 3) };
+Point gradientFunction(Point x) {
+    return{ 4 * (x.x1 - 1), 6 * (x.x2 - 3) };
 }
 
-Point gradientH(Point X) {
-    return{ 1, 1 };
+Point gradientH(Point x) {
+    return{ 1, 1};
 }
 
-Point gradientG(Point X) {
-    return{ 2 * X.x1, 2 * X.x2 };
+Point gradientG(Point x) {
+    return{ 2 * x.x1, 2 * x.x2 };
 }
 
-Point Shtrafgradient(Point X, double r) // градиент штрафной функции
+Point Shtrafgradient(Point x, double r) // градиент штрафной функции
 {
     Point gFi;
-    gFi.x1 = H(X) * gradientH(X).x1 / r - 2 * r * gradientG(X).x1 / (G(X) * G(X)); // Исправлено знак
-    gFi.x2 = H(X) * gradientH(X).x2 / r - 2 * r * gradientG(X).x2 / (G(X) * G(X)); // Исправлено знак
+    gFi.x1 = H(x) * gradientH(x).x1 / r - 2 * r * gradientG(x).x1 / (G(x) * G(x));
+    gFi.x2 = H(x) * gradientH(x).x2 / r - 2 * r * gradientG(x).x2 / (G(x) * G(x));
     return gFi;
 }
 
-Point gradientP(Point X, double r)
+Point gradientP(Point x, double r)
 {
-    return{ gradientF(X).x1 + Shtrafgradient(X, r).x1, gradientF(X).x2 + Shtrafgradient(X, r).x2 };
+    return{ gradientFunction(x).x1 + Shtrafgradient(x, r).x1, gradientFunction(x).x2 + Shtrafgradient(x, r).x2 };
 }
 
-double Norma(Point X)
+double Norma(Point x)
 {
-    return sqrt(X.x1 * X.x1 + X.x2 * X.x2);
+    return sqrt(x.x1 * x.x1 + x.x2 * x.x2);
 }
 
 Point GradientSpusk(Point p0, double r)
 {
-    Point X, gf = gradientP(p0, r);
-    int k_local = 0; // Исправлено имя переменной для избежания конфликтов
+    Point x, gf = gradientP(p0, r);
+    int k_local = 0;
     double t = 1.0;
     do
     {
         k_local++;
 
-        X.x1 = p0.x1 - t * gf.x1;
-        X.x2 = p0.x2 - t * gf.x2;
+        x.x1 = p0.x1 - t * gf.x1;
+        x.x2 = p0.x2 - t * gf.x2;
 
-        if ((P(X, r) - P(p0, r)) >= 0) t = t / 2;
-        p0 = X;
+        if ((P(x, r) - P(p0, r)) >= 0) t = t / 2;
+        p0 = x;
         gf = gradientP(p0, r);
 
     } while (Norma(gf) > epsilon2);
 
-    return X;
+    return x;
+}
+void IterationProcess(Point& p0, Point& x) {
+    do {
+        k++;
+        x = GradientSpusk(p0, r);
+        fi = F(x, r);
+        r = r / C;
+        p0 = x; // Обновление начальной точки для следующей итерации
+    } while (abs(fi) > epsilon1);
 }
 
 int main()
 {
     setlocale(LC_ALL, "rus");
-    Point p0, X;
+    Point p0, x;
 
-    p0.x1 = 1, p0.x2 = 1; // Начальные значения
+    p0.x1 = 1, p0.x2 = 1; // Начальное приближение
 
-    do
-    {
-        k++;
-        X = GradientSpusk(p0, r);
-        fi = F(X, r);
-        r = r / C;
-        p0 = X; // Обновление начальной точки для следующей итерации
-    } while (abs(fi) > epsilon1);
+    IterationProcess(p0, x);
 
-    cout << "Точка условного экстремума =  " << round(X.x1) << ", " << round(X.x2) << "\n";
+    cout << "Точка условного экстремума =  " << round(x.x1) << ", " << round(x.x2) << "\n";
     cout << "Количество итераций K = " << k << endl;
-    //cout << "function = " << round(function(X)) << endl;
-    //cout << "Проверка:\n" << "h = " << H(X) << endl;
-    //cout << "g = " << G(X) << endl;
-
     return 0;
 }
